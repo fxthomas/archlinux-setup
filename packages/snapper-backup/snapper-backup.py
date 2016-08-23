410455ae84a0ba48c6d2307c743bdd7642e78096
François-Xavier Thomas <fx.thomas@gmail.com>
2016-08-23 02:06:45 +0200

    More error handling to snapper backup

--- packages/snapper-backup/snapper-backup.py
+++ packages/snapper-backup/snapper-backup.py
@@ -20,23 +20,29 @@ parser.add_argument("src", help="Source location")
 parser.add_argument("dst", help="Destination location")
 args = parser.parse_args()
 
+# Cleanup temporary snapshots
 tmp_path = os.path.join(args.dst, "snapshot")
 if os.path.exists(tmp_path):
     print("Temporary snapshot %s still exists, removing...")
     subprocess.check_call(
         ["btrfs", "subvolume", "delete", "--commit-after", tmp_path])
 
+# List source and destination snapshots
 src_snapshots = os.listdir(args.src)
 dst_snapshots = os.listdir(args.dst)
 
+# Find out already backed-up and missing snapshots
 common = set(src_snapshots).intersection(dst_snapshots)
 missing = set(src_snapshots).difference(dst_snapshots)
 
+# Backup each missing snapshot
 for name in missing:
-
 
     src_path = os.path.join(args.src, name, "snapshot")
     dst_path = os.path.join(args.dst, name)
+
+    if not os.path.exists(src_path):
+        continue
 
     print("Sending snapshot %s to %s..." % (name, args.dst))
 
@@ -48,8 +54,12 @@ for name in missing:
     try:
         proc_send = subprocess.Popen(cmd_send, stdout=subprocess.PIPE)
         proc_recv = subprocess.Popen(cmd_recv, stdin=proc_send.stdout)
-        proc_send.wait()
-        proc_recv.wait()
+        ret_send = proc_send.wait()
+        ret_recv = proc_recv.wait()
+        if ret_send > 0:
+            raise Exception("The `btrfs send` process exited with code %d" % ret_send)
+        if ret_recv > 0:
+            raise Exception("The `btrfs receive` process exited with code %d" % ret_recv)
     except:
         subprocess.check_call(["btrfs", "subvolume", "delete", tmp_path])
         raise
